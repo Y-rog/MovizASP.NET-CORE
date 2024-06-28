4388794f1dd482c19dc8cbcdd90aa9a29251e7c3
Y-rog <fulgueiras.gregory@gmail.com>
2024-06-28 23:14:52 +0200

    first commit

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using Moviz.Models;
+
+namespace Moviz.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Créer un film
+            List<MovieModel> allMovies = new ()
+            {
+                new MovieModel {
+                Id = 1,
+                Name = "Le seigneur des anneaux : la communauté de l'anneau",
+                ReleaseDate = new DateTime(2001)
+                },
+                new MovieModel {
+                Id = 2,
+                Name = "Le seigneur des anneaux : les deux tours",
+                ReleaseDate = new DateTime(2002)
+                },
+                new MovieModel {
+                Id = 3,
+                Name = "Le seigneur des anneaux : le retour du roi",
+                ReleaseDate = new DateTime(2003)
+                },
+            };
+
+            //Vue Views/Movie/index.cshtml et donne lui l'objet myFilm
+            return View(nameof(Index), allMovies);
+        }
+    }
+}
